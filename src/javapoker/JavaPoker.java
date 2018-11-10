176994c3970c1c4f561f108a78cc4a1082a5d8f2
rarun321 <43283635+rarun321@users.noreply.github.com>
2018-11-10 10:05:29 -0700

    Initial Commits

---
+++ src/javapoker/JavaPoker.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javapoker;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author rithv
+ */
+public class JavaPoker {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JFrame mainFrame = new JFrame();
+        mainFrame.setSize(2000, 2000);
+        mainFrame.setVisible(true);
+    }
+    
+}
